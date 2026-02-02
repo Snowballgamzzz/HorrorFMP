fae07e5c54553158ea3fe08c7665875a791aa12d
Snowballgamzzz <2winga47@solent.ac.uk>
2026-02-02 21:41:48 +0000

    Added player movement
    Added player movement. (Walking, looking, jumping, sprinting)

---
+++ HorrorProject/Assets/Scripts/Player/Movement/FP Jumping.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(FPController))]
+public class FPJumping : MonoBehaviour
+{
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float jumpPressBufferTime = 0.05f;
+    [SerializeField] float jumpGroundGraceTime = 0.2f;
+
+    FPController controller;
+    PlayerInput playerInput;
+    InputAction jumpAction;
+
+    public bool tryingToJump;
+    float lastJumpPressTime;
+    float lastGroundedTime;
+    int jumps;
+
+    void Awake()
+    {
+        controller = GetComponent<FPController>();
+    }
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        jumpAction = playerInput.actions["jump"];
+        jumpAction.performed += OnJump;
+    }
+
+    void OnEnable()
+    {
+        controller.OnBeforeMove += OnBeforeMove;
+        controller.OnGroundStateChange += OnGroundStateChange;
+        if (playerInput != null)
+        {
+            jumpAction.performed += OnJump;
+        }
+    }
+
+    void OnDisable()
+    {
+        controller.OnBeforeMove -= OnBeforeMove;
+        controller.OnGroundStateChange -= OnGroundStateChange;
+        if (playerInput != null)
+        {
+            jumpAction.performed -= OnJump;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        tryingToJump = true;
+        lastJumpPressTime = Time.time;
+    }
+
+    void OnBeforeMove()
+    {
+        if (controller.IsGrounded) jumps = 0;
+
+        bool wasTryingToJump = Time.time - lastJumpPressTime < jumpPressBufferTime;
+        bool wasGrounded = Time.time - lastGroundedTime < jumpGroundGraceTime;
+
+        bool isOrWasTryingToJump = tryingToJump || (wasTryingToJump && controller.IsGrounded);
+        bool isOrWasGrounded = controller.IsGrounded || wasGrounded;
+
+        bool jumpAllowed = jumps < 1;
+
+        bool canJump = jumpAllowed && isOrWasTryingToJump && isOrWasGrounded;
+
+        if (canJump)
+        {
+            controller.velocity.y += jumpSpeed;
+            jumps++;
+        }
+        tryingToJump = false;
+    }
+
+    void OnGroundStateChange(bool isGrounded)
+    {
+        if (!isGrounded) lastGroundedTime = Time.time;
+    }
+}
